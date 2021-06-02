475bc308d74379f39b3789626b77baadc6aaba46
mbatoul <batoulmathis@gmail.com>
2021-06-02 18:17:16 +0200

    Pass `pandas` dataframes to `fit` and `evaluate `instead of `numpy` arrays.

--- sklearn_benchmarks/tf_decision_forests.py
+++ sklearn_benchmarks/tf_decision_forests.py
@@ -1,5 +1,5 @@
-# Load TensorFlow Decision Forests
 import tensorflow_decision_forests as tfdf
+import pandas as pd
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 
@@ -12,21 +12,20 @@ X, y = make_classification(
     random_state=42,
 )
 
-X_train, y_train, X_test, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
+X_train = pd.DataFrame(X_train)
 X_train["label"] = y_train
+
+X_test = pd.DataFrame(X_test)
 X_test["label"] = y_test
 
-# Convert the pandas dataframe into a TensorFlow dataset
-train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(X_train, label="species")
+train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(X_train, label="label")
 
-# Train the model
 model = tfdf.keras.RandomForestModel()
 model.fit(train_ds)
 
-# Convert it to a TensorFlow dataset
-test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(X_test, label="species")
+test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(X_test, label="label")
 
-# Evaluate the model
 model.compile(metrics=["accuracy"])
 print(model.evaluate(test_ds))
