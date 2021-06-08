ffa9f08851d3645029ec7544717b29e73fdfb100
mbatoul <batoulmathis@gmail.com>
2021-06-08 10:28:02 +0200

    Small fix time report + env info.

--- sklearn_benchmarks/report.py
+++ sklearn_benchmarks/report.py
@@ -38,13 +38,13 @@ def print_time_report():
 
     display(Markdown("# Time report"))
     for index, row in df.iterrows():
-        display(Markdown("%s: %ih %im %is" % (index.capitalize(), *row.values)))
+        display(Markdown("**%s**: %ih %im %is" % (index, *row.values)))
 
 
 def print_env_info():
     with open(ENV_INFO_PATH) as json_file:
         data = json.load(json_file)
-    display(Markdown("# Benchmark environment information"))
+    display(Markdown("# Benchmark environment"))
     print(json.dumps(data, indent=2))
 
 
