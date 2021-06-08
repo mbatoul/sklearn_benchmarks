3ae9f9042ef7cbf84cb06a972e68ea5e3223671a
mbatoul <batoulmathis@gmail.com>
2021-06-08 17:21:02 +0200

    WIP TFDF benchmark script.

--- sklearn_benchmarks/tf_decision_forests.py
+++ sklearn_benchmarks/tf_decision_forests.py
@@ -1,31 +1,43 @@
-import tensorflow_decision_forests as tfdf
+# import tensorflow_decision_forests as tfdf
+import numpy as np
 import pandas as pd
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
+import time
+from sklearn import metrics
+from sklearn.ensemble import HistGradientBoostingClassifier
 
-X, y = make_classification(
-    n_samples=100_000,
-    n_features=1000,
-    n_classes=5,
-    n_informative=5,
-    n_redundant=0,
-    random_state=42,
-)
+df_train = pd.read_csv("https://s3.amazonaws.com/benchm-ml--main/train-1m.csv")
+df_test = pd.read_csv("https://s3.amazonaws.com/benchm-ml--main/test.csv")
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+df_train["dep_delayed_15min"] = np.where(df_train["dep_delayed_15min"] == "Y", 1, 0)
+df_test["dep_delayed_15min"] = np.where(df_test["dep_delayed_15min"] == "Y", 1, 0)
 
-X_train = pd.DataFrame(X_train)
-X_train["label"] = y_train
+# scikit-learn
+X_train = df_train.drop(columns=["dep_delayed_15min"], axis=1).to_numpy()
+y_train = df_train[["dep_delayed_15min"]].to_numpy()
+X_test = df_test.drop(columns=["dep_delayed_15min"], axis=1).to_numpy()
+y_test = df_test[["dep_delayed_15min"]].to_numpy()
 
-X_test = pd.DataFrame(X_test)
-X_test["label"] = y_test
+model = HistGradientBoostingClassifier()
+start = time.perf_counter()
+model.fit(X_train, y_train)
+end = time.perf_counter()
+time_elapsed = round(end - start, 5)
+print(f"time scikit-learn: {end - start}s")
 
-train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(X_train, label="label")
+y_pred = model.predict(X_test)
+print(metrics.roc_auc_score(y_test, y_pred))
 
-model = tfdf.keras.RandomForestModel()
-model.fit(train_ds)
+# TensorFlow Decision Forests
+dtf_train = tfdf.keras.pd_dataframe_to_tf_dataset(df_train, label="dep_delayed_15min")
+dtf_test = tfdf.keras.pd_dataframe_to_tf_dataset(df_test, label="dep_delayed_15min")
 
-test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(X_test, label="label")
+model = tfdf.keras.GradientBoostedTreesModel()
+start = time.perf_counter()
+model.fit(x=dtf_train)
+end = time.perf_counter()
+time_elapsed = round(end - start, 5)
+print(f"time scikit-learn: {end - start}s")
 
-model.compile(metrics=["accuracy"])
-print(model.evaluate(test_ds))
+y_pred = model.predict(dtf_test)
+y_test = df_test["dep_delayed_15min"]
+print(metrics.roc_auc_score(y_test, y_pred))
