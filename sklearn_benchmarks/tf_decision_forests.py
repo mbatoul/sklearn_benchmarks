5f92c19e4d66a9b72ea5963e683f70a50749dcb1
mbatoul <batoulmathis@gmail.com>
2021-06-08 23:31:13 +0200

    Encode categorical features.
    Run models 10 times and take averages.

--- sklearn_benchmarks/tf_decision_forests.py
+++ sklearn_benchmarks/tf_decision_forests.py
@@ -4,40 +4,60 @@ import pandas as pd
 import time
 from sklearn import metrics
 from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.preprocessing import LabelEncoder
 
 df_train = pd.read_csv("https://s3.amazonaws.com/benchm-ml--main/train-1m.csv")
 df_test = pd.read_csv("https://s3.amazonaws.com/benchm-ml--main/test.csv")
 
 df_train["dep_delayed_15min"] = np.where(df_train["dep_delayed_15min"] == "Y", 1, 0)
 df_test["dep_delayed_15min"] = np.where(df_test["dep_delayed_15min"] == "Y", 1, 0)
+cat_cols = ["Month", "DayofMonth", "DayOfWeek", "UniqueCarrier", "Origin", "Dest"]
+
+df_train[cat_cols] = df_train[cat_cols].apply(LabelEncoder().fit_transform)
+df_test[cat_cols] = df_test[cat_cols].apply(LabelEncoder().fit_transform)
 
 # scikit-learn
 X_train = df_train.drop(columns=["dep_delayed_15min"], axis=1).to_numpy()
-y_train = df_train[["dep_delayed_15min"]].to_numpy()
+y_train = df_train["dep_delayed_15min"].values
 X_test = df_test.drop(columns=["dep_delayed_15min"], axis=1).to_numpy()
-y_test = df_test[["dep_delayed_15min"]].to_numpy()
+y_test = df_test["dep_delayed_15min"].values
 
-model = HistGradientBoostingClassifier()
-start = time.perf_counter()
-model.fit(X_train, y_train)
-end = time.perf_counter()
-time_elapsed = round(end - start, 5)
-print(f"time scikit-learn: {end - start}s")
+times = []
+scores = []
+for _ in range(10):
+    model = HistGradientBoostingClassifier()
+    start = time.perf_counter()
+    model.fit(X_train, y_train)
+    end = time.perf_counter()
 
-y_pred = model.predict(X_test)
-print(metrics.roc_auc_score(y_test, y_pred))
+    time_elapsed = round(end - start, 5)
+    times.append(time_elapsed)
+
+    y_pred = model.predict(X_test)
+    score = round(metrics.roc_auc_score(y_test, y_pred), 3)
+    scores.append(score)
+
+print(f"average score scikit-learn: {np.mean(scores)}")
+print(f"average time scikit-learn: {np.mean(times)}s")
 
 # TensorFlow Decision Forests
-dtf_train = tfdf.keras.pd_dataframe_to_tf_dataset(df_train, label="dep_delayed_15min")
+dtf_train = tfdf.keras.pd_dataframe_to_tf_dataset(df_train, label="dep_delayed_13min")
 dtf_test = tfdf.keras.pd_dataframe_to_tf_dataset(df_test, label="dep_delayed_15min")
 
-model = tfdf.keras.GradientBoostedTreesModel()
-start = time.perf_counter()
-model.fit(x=dtf_train)
-end = time.perf_counter()
-time_elapsed = round(end - start, 5)
-print(f"time scikit-learn: {end - start}s")
+times = []
+scores = []
+for _ in range(10):
+    model = tfdf.keras.GradientBoostedTreesModel()
+    start = time.perf_counter()
+    model.fit(x=dtf_train)
+    end = time.perf_counter()
 
-y_pred = model.predict(dtf_test)
-y_test = df_test["dep_delayed_15min"]
-print(metrics.roc_auc_score(y_test, y_pred))
+    time_elapsed = round(end - start, 5)
+    times.append(time_elapsed)
+
+    y_pred = model.predict(dtf_test)
+    score = round(metrics.roc_auc_score(y_test, y_pred), 3)
+    scores.append(score)
+
+print(f"average score tensorflow: {np.mean(scores)}")
+print(f"average time tensorflow: {np.mean(times)}s")
