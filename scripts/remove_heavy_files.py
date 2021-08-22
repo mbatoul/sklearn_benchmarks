77ac2a5b9b60c75fcde5e36c4f1fef11374fd5c8
mbatoul <batoulmathis@gmail.com>
2021-08-22 19:49:39 +0200

    Fix files path in Python script.

--- scripts/remove_heavy_files.py
+++ scripts/remove_heavy_files.py
@@ -1,9 +1,9 @@
-import os
+from sklearn_benchmarks.config import PROFILING_RESULTS_PATH
 
 threshold_in_bytes = 1e8  # 100mb
 
-files = os.listdir("./results/profiling")
+files = list(PROFILING_RESULTS_PATH.glob("*.html"))
 for file in files:
-    if os.path.getsize(file) >= threshold_in_bytes:
-        print(f"removing {file}...")
-        os.remove(file)
+    if file.stat().st_size >= threshold_in_bytes:
+        print(f"removing {file} (size {file.stat().st_size} bytes)...")
+        file.unlink()
