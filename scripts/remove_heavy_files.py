d01bb9da5ae9ae4bd1b38e04e9932cc648f50194
mbatoul <batoulmathis@gmail.com>
2021-08-25 15:20:21 +0200

    Fix script that removes heavy files.

--- scripts/remove_heavy_files.py
+++ scripts/remove_heavy_files.py
@@ -1,12 +1,15 @@
 from pathlib import Path
+import os
 
 if __name__ == "__main__":
     threshold_in_bytes = 1e8  # 100mb
     profiling_results_path = (
         Path(__file__).resolve().parent.parent / "results" / "profiling"
     )
-    files = list(profiling_results_path.glob("*.html"))
+    files = []
+    for extension in ["html", "json.gz"]:
+        files += list(profiling_results_path.glob(f"*.{extension}"))
     for file in files:
         if file.stat().st_size >= threshold_in_bytes:
             print(f"removing {file} (size {file.stat().st_size} bytes)...")
-            file.unlink()
+            os.remove(file)
